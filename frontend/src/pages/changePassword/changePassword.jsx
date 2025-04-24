121ef25cd7abf313da0e07b6368d7112562dedd9
Jaygupta930 <jayguptajaygupta8@gmail.com>
2025-04-25 00:28:36 +0530

    Add ChangePassword component with form structure and placeholder

---
+++ frontend/src/pages/changePassword/changePassword.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ChangePassword = () => {
+
+  return (
+    <div>
+      <h2>Change Password</h2>
+      <form>
+        <p>Change password form placeholder.</p>
+        <button type="submit">Update Password</button>
+      </form>
+    </div>
+  );
+};
+
+export default ChangePassword;
